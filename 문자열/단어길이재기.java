ff682abf43e8219f46b0623e61ea710b438f87ec
HyeonJi Na <dev.hyeonji@gmail.com>
2023-05-04 09:40:13 +0900

    2일차

---
+++ 문자열/단어길이재기.java
@@ -0,0 +1,14 @@
+package 문자열;
+import java.io.*;
+import java.util.*;
+
+public class 단어길이재기 {
+    public static void main (String[] args) throws IOException{
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+
+        String a = in.readLine();
+		System.out.println(a.length());
+        
+	}
+}
+
